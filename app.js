a11b95fec8fdf27a3154f6bd226200512f8cffac
TabethaSnead <tabetha.snead@gmail.com>
2023-01-06 14:52:27 -0800

    added elements to HTML, used getElementById to connect to JS and added 1 event listener

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-/* Imports */
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 
-/* Get DOM Elements */
+const questionInput = document.getElementById('question-input');
+const submitButton = document.getElementById('submit-button');
 
-/* State */
-
-/* Events */
-
-/* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
+submitButton.addEventListener('click', () => {
+    console.log('click');
+});
